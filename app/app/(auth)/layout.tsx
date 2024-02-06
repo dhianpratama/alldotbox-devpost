6f0e737ed40c23f32965ba4e90feec79f2914bcf
mohaghali <chemimohamed8@gmail.com>
2024-02-06 06:20:54 +0100

    edit login box

--- app/app/(auth)/layout.tsx
+++ app/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
@@ -9,6 +10,14 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
       {children}
+      <Image
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="absolute left-0 top-0 -z-10 h-screen w-screen object-fill"
+        src="/nm_background.jpg"
+        alt=""
+      />
     </div>
   );
 }
