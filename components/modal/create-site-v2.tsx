3522fd8556be47cd60fd0c1775daf997c7203868
Shubham Patel <shubham.pipaliya55@gmail.com>
2024-02-15 18:02:46 +0530

    fix disable issue

--- components/modal/create-site-v2.tsx
+++ components/modal/create-site-v2.tsx
@@ -70,7 +70,6 @@ export default function CreateSiteModalV2({
             maxLength={32}
             required
             readOnly={true}
-            disabled={true}
             className="w-full rounded-md border border-stone-200 bg-stone-50 px-4 py-2 text-sm text-stone-600 placeholder:text-stone-400 focus:border-black focus:outline-none focus:ring-black dark:border-stone-600 dark:bg-black dark:text-white dark:placeholder-stone-700 dark:focus:ring-white"
           />
         </div>
@@ -104,7 +103,6 @@ export default function CreateSiteModalV2({
               maxLength={32}
               required
               readOnly
-              disabled={true}
               className="w-full rounded-l-lg border border-stone-200 bg-stone-50 px-4 py-2 text-sm text-stone-600 placeholder:text-stone-400 focus:border-black focus:outline-none focus:ring-black dark:border-stone-600 dark:bg-black dark:text-white dark:placeholder-stone-700 dark:focus:ring-white"
             />
             <div className="flex items-center rounded-r-lg border border-l-0 border-stone-200 bg-stone-100 px-3 text-sm dark:border-stone-600 dark:bg-stone-800 dark:text-stone-400">
