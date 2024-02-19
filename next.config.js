a24efcb3d3ce699ac591d2cad204ba3109d2b251
Shubham Patel <shubham.pipaliya55@gmail.com>
2024-02-19 10:54:26 +0530

    fix commna

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   reactStrictMode:false,
   experimental: {
     serverActions: {
-      allowedOrigins: ["app.localhost:3000", "secondcourt.com", "alldotbox.com"."all.box"],
+      allowedOrigins: ["app.localhost:3000", "secondcourt.com", "alldotbox.com","all.box"],
     },
   },
   images: {
